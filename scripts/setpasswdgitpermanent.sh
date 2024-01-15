64280f260db27d53fc7e94ba93b5c9664d0101b3
Prem Dhadkar <prem.dhadkar@gmail.com>
2024-01-15 23:26:26 +0530

    add a script for permanent password injection into git cli

---
+++ scripts/setpasswdgitpermanent.sh
@@ -0,0 +1,11 @@
+git config --global user.name Prem Dhadkar
+
+git config --global user.email prem.dhadkar@gmail.com
+
+cd ~
+
+echo https://premdhadkar:password@github.com >> .git-credentials
+
+git config --global credential.helper store
+
+
